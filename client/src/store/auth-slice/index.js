c1bf930e3dd1fc1f6fd0747e6315dd908f906b89
satyam9315170823 <satyam.kumar@acem.edu.in>
2025-01-04 01:18:24 +0530

    bye

--- client/src/store/auth-slice/index.js
+++ client/src/store/auth-slice/index.js
@@ -1,6 +1,5 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
-import Cookies from 'js-cookie';
 
 const initialState = {
   isAuthenticated: false,
@@ -26,6 +25,7 @@ export const registerUser = createAsyncThunk(
 
 export const loginUser = createAsyncThunk(
   "/auth/login",
+
   async (formData) => {
     const response = await axios.post(
       "http://localhost:5000/api/auth/login",
@@ -35,37 +35,14 @@ export const loginUser = createAsyncThunk(
       }
     );
 
-    if (response.data.success) {
-      // Store the token in a cookie on successful login
-      Cookies.set("token", response.data.token, { expires: 7 }); // Expires in 7 days
-    }
-
     return response.data;
   }
 );
 
-// export const loginUser = createAsyncThunk(
-//   "/auth/login",
-
-//   async (formData) => {
-//     const response = await axios.post(
-//       "http://localhost:5000/api/auth/login",
-//       formData,
-//       {
-//         withCredentials: true,
-//       }
-//     );
-
-//     return response.data;
-//   }
-// );
-
 export const logoutUser = createAsyncThunk(
   "/auth/logout",
-  async () => {
-    // Remove token from cookies
-    Cookies.remove("token");
 
+  async () => {
     const response = await axios.post(
       "http://localhost:5000/api/auth/logout",
       {},
@@ -78,67 +55,25 @@ export const logoutUser = createAsyncThunk(
   }
 );
 
-// export const logoutUser = createAsyncThunk(
-//   "/auth/logout",
-
-//   async () => {
-//     const response = await axios.post(
-//       "http://localhost:5000/api/auth/logout",
-//       {},
-//       {
-//         withCredentials: true,
-//       }
-//     );
-
-//     return response.data;
-//   }
-// );
-
-// export const checkAuth = createAsyncThunk(
-//   "/auth/checkauth",
-
-//   async () => {
-//     const response = await axios.get(
-//       "http://localhost:5000/api/auth/check-auth",
-//       {
-//         withCredentials: true,
-//         headers: {
-//           "Cache-Control":
-//             "no-store, no-cache, must-revalidate, proxy-revalidate",
-//         },
-//       }
-//     );
-
-//     return response.data;
-//   }
-// );
-
 export const checkAuth = createAsyncThunk(
   "/auth/checkauth",
+
   async () => {
-    // Get the token from the cookie
-    const token = Cookies.get("token");
-
-    if (!token) {
-      // If no token exists, return early with an error or indicate the user is not authenticated
-      return { success: false };
-    }
-
-    // If token exists, make the API call to verify its validity
     const response = await axios.get(
       "http://localhost:5000/api/auth/check-auth",
       {
+        withCredentials: true,
         headers: {
-          Authorization: `Bearer ${token}`,  // Pass token in the Authorization header
           "Cache-Control":
             "no-store, no-cache, must-revalidate, proxy-revalidate",
         },
       }
     );
 
-    return response.data;  // Return the response from the API call
+    return response.data;
   }
 );
+
 const authSlice = createSlice({
   name: "auth",
   initialState,
--- server/controllers/auth/auth-controller.js
+++ server/controllers/auth/auth-controller.js
@@ -1,123 +1,3 @@
-// const bcrypt = require("bcryptjs");
-// const jwt = require("jsonwebtoken");
-// const User = require("../../models/User");
-
-// //register
-// const registerUser = async (req, res) => {
-//   const { userName, email, password } = req.body;
-
-//   try {
-//     const checkUser = await User.findOne({ email });
-//     if (checkUser)
-//       return res.json({
-//         success: false,
-//         message: "User Already exists with the same email! Please try again",
-//       });
-
-//     const hashPassword = await bcrypt.hash(password, 12);
-//     const newUser = new User({
-//       userName,
-//       email,
-//       password: hashPassword,
-//     });
-
-//     await newUser.save();
-//     res.status(200).json({
-//       success: true,
-//       message: "Registration successful",
-//     });
-//   } catch (e) {
-//     console.log(e);
-//     res.status(500).json({
-//       success: false,
-//       message: "Some error occured",
-//     });
-//   }
-// };
-
-// //login
-// const loginUser = async (req, res) => {
-//   const { email, password } = req.body;
-
-//   try {
-//     const checkUser = await User.findOne({ email });
-//     if (!checkUser)
-//       return res.json({
-//         success: false,
-//         message: "User doesn't exists! Please register first",
-//       });
-
-//     const checkPasswordMatch = await bcrypt.compare(
-//       password,
-//       checkUser.password
-//     );
-//     if (!checkPasswordMatch)
-//       return res.json({
-//         success: false,
-//         message: "Incorrect password! Please try again",
-//       });
-
-//     const token = jwt.sign(
-//       {
-//         id: checkUser._id,
-//         role: checkUser.role,
-//         email: checkUser.email,
-//         userName: checkUser.userName,
-//       },
-//       "CLIENT_SECRET_KEY",
-//       { expiresIn: "60m" }
-//     );
-
-//     res.cookie("token", token, { httpOnly: true, secure: true }).json({
-//       success: true,
-//       message: "Logged in successfully",
-//       user: {
-//         email: checkUser.email,
-//         role: checkUser.role,
-//         id: checkUser._id,
-//         userName: checkUser.userName,
-//       },
-//     });
-//   } catch (e) {
-//     console.log(e);
-//     res.status(500).json({
-//       success: false,
-//       message: "Some error occured",
-//     });
-//   }
-// };
-
-// //logout
-
-// const logoutUser = (req, res) => {
-//   res.clearCookie("token").json({
-//     success: true,
-//     message: "Logged out successfully!",
-//   });
-// };
-
-// //auth middleware
-// const authMiddleware = async (req, res, next) => {
-//   const token = req.cookies.token;
-//   if (!token)
-//     return res.status(401).json({
-//       success: false,
-//       message: "Unauthorised user!",
-//     });
-
-//   try {
-//     const decoded = jwt.verify(token, "CLIENT_SECRET_KEY");
-//     req.user = decoded;
-//     next();
-//   } catch (error) {
-//     res.status(401).json({
-//       success: false,
-//       message: "Unauthorised user!",
-//     });
-//   }
-// };
-
-// module.exports = { registerUser, loginUser, logoutUser, authMiddleware };
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const User = require("../../models/User");
@@ -150,7 +30,7 @@ const registerUser = async (req, res) => {
     console.log(e);
     res.status(500).json({
       success: false,
-      message: "Some error occurred",
+      message: "Some error occured",
     });
   }
 };
@@ -164,10 +44,13 @@ const loginUser = async (req, res) => {
     if (!checkUser)
       return res.json({
         success: false,
-        message: "User doesn't exist! Please register first",
+        message: "User doesn't exists! Please register first",
       });
 
-    const checkPasswordMatch = await bcrypt.compare(password, checkUser.password);
+    const checkPasswordMatch = await bcrypt.compare(
+      password,
+      checkUser.password
+    );
     if (!checkPasswordMatch)
       return res.json({
         success: false,
@@ -185,12 +68,7 @@ const loginUser = async (req, res) => {
       { expiresIn: "60m" }
     );
 
-    // Set the JWT token in the cookie with secure configuration
-    res.cookie("token", token, {
-      httpOnly: true, // Prevents client-side access to the cookie
-      sameSite:"strict",
-      secure: process.env.NODE_ENV === "production",  // Only true in production for HTTPS
-    }).json({
+    res.cookie("token", token, { httpOnly: true, secure: true }).json({
       success: true,
       message: "Logged in successfully",
       user: {
@@ -204,12 +82,13 @@ const loginUser = async (req, res) => {
     console.log(e);
     res.status(500).json({
       success: false,
-      message: "Some error occurred",
+      message: "Some error occured",
     });
   }
 };
 
 //logout
+
 const logoutUser = (req, res) => {
   res.clearCookie("token").json({
     success: true,
