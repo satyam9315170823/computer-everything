67e11cc3f2b505ae3e38fa9d1b91f6b445c2075f
satyam9315170823 <satyam.kumar@acem.edu.in>
2025-01-04 00:48:43 +0530

    jhbjhv

--- server/server.js
+++ server/server.js
@@ -30,7 +30,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: process.env.CLIENT_URL ,
+    origin:"https://everythingcomputers.in" ,
     methods: ["GET", "POST", "DELETE", "PUT"],
     allowedHeaders: [
       "Content-Type",
